e9f546a483a31f132d6c941744573b27f0358510
André Ruffert <a.ruffert@gmail.com>
2012-04-24 21:31:34 +0200

    Convert Indention to Spaces

--- placeholder.js
+++ placeholder.js
@@ -6,69 +6,69 @@
 
 
 (function() {
-	var Polyfill			= this.Polyfill || {},
-		PlaceholderSupport	= this.PlaceholderSupport || {
-			'input': 'placeholder' in document.createElement('input'),
-			'textarea': 'placeholder' in document.createElement('textarea')
-		}
-	;
+    var Polyfill            = this.Polyfill || {},
+        PlaceholderSupport  = this.PlaceholderSupport || {
+            'input': 'placeholder' in document.createElement('input'),
+            'textarea': 'placeholder' in document.createElement('textarea')
+        }
+    ;
 
-	Polyfill.Placeholder = new Class({
-		Implements: [Options],
-		options: {
-			className: 'js-placeholder'
-		},
+    Polyfill.Placeholder = new Class({
+        Implements: [Options],
+        options: {
+            className: 'js-placeholder'
+        },
 
-		initialize: function(element, index, options)
-		{
-			this.index			= index;
-			this.element		= element;
-			this.elementType	= this.element.get('tag');
+        initialize: function(element, index, options)
+        {
+            this.index          = index;
+            this.element        = element;
+            this.elementType    = this.element.get('tag');
 
-			if (PlaceholderSupport[this.elementType] === false) {
-				this.setOptions(options);
-				this.createPlaceholder();
-				this.attachEvents();
-			}
-		},
+            if (PlaceholderSupport[this.elementType] === false) {
+                this.setOptions(options);
+                this.createPlaceholder();
+                this.attachEvents();
+            }
+        },
 
-		attachEvents: function()
-		{
-			var self		= this,
-				placeholder = this.element.getParent().getElements('label').pop()
-			;
-			this.element.addEvents({
-				focus: function()
-				{
-					if (placeholder.getStyle('display') === 'block') {
-						placeholder.setStyle('display', 'none');
-					}
-				},
-				blur: function()
-				{
-					if (self.element.get('value') === '') {
-						placeholder.setStyle('display', 'block');
-					}
-				}
-			});
-		},
+        attachEvents: function()
+        {
+            var self        = this,
+                placeholder = this.element.getParent().getElements('label').pop()
+            ;
+            this.element.addEvents({
+                focus: function()
+                {
+                    if (placeholder.getStyle('display') === 'block') {
+                        placeholder.setStyle('display', 'none');
+                    }
+                },
+                blur: function()
+                {
+                    if (self.element.get('value') === '') {
+                        placeholder.setStyle('display', 'block');
+                    }
+                }
+            });
+        },
 
-		createPlaceholder: function()
-		{	
-			var state = this.element.get('value') === '' ? 'block' : 'none';
+        createPlaceholder: function()
+        {   
+            var state = this.element.get('value') === '' ? 'block' : 'none';
 
-			if (!this.element.get('id')) {
-				this.element.set('id', 'js-placeholder-' + this.index);
-			}
-			var placeholder = new Element('label', {
-				'for': this.element.get('id'),
-				'class': this.options.className,
-				'styles': { 'display': state },
-				'html': this.element.get('placeholder')
-			});
-			this.element.getParent().adopt(placeholder);
-		}
-	});
+            if (!this.element.get('id')) {
+                this.element.set('id', 'js-placeholder-' + this.index);
+            }
+            var placeholder = new Element('label', {
+                'for': this.element.get('id'),
+                'class': this.options.className,
+                'styles': { 'display': state },
+                'html': this.element.get('placeholder')
+            });
+            this.element.getParent().adopt(placeholder);
+        }
+    });
 
-	this.Polyfill = Polyfill;
+    this.Polyfill = Polyfill;
 })();
